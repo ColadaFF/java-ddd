a65cc3989dfe4ea6c42c5bdf91c16412fca0b66a
Cristian Barrientos <cbarrientos@ias.com.co>
2021-12-17 11:06:37 -0500

    Clean up

--- src/main/java/co/com/ias/hairs/modules/seats/core/services/CreateAppointmentService.java
+++ src/main/java/co/com/ias/hairs/modules/seats/core/services/CreateAppointmentService.java
@@ -4,6 +4,7 @@ import co.com.ias.hairs.modules.seats.core.domain.Appointment;
 import co.com.ias.hairs.modules.seats.core.domain.DayShift;
 import co.com.ias.hairs.modules.seats.core.domain.DayShiftCommandError;
 import co.com.ias.hairs.modules.seats.core.domain.SeatNumber;
+import com.google.common.base.Preconditions;
 import io.vavr.control.Either;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,10 @@ public class CreateAppointmentService {
 
 
     public Either<DayShiftCommandError, DayShift> execute(SeatNumber seatNumber, LocalDate date, Appointment appointment) {
+        Preconditions.checkNotNull(seatNumber, "seatNumber can not be null");
+        Preconditions.checkNotNull(date, "date can not be null");
+        Preconditions.checkNotNull(appointment, "appointment can not be null");
+
         return Either.right(null);
     }
 }
